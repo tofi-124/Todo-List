f736c30fca47bd5b7e7ff0b87cd71273c5a93ba3
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-04 18:54:36 +0000

    refactored with OOP concepts

---
+++ index.js
@@ -0,0 +1,34 @@
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    logState: function () {
+      console.log(
+        `${this.title} has${this.complete ? " " : " not "}been completed`
+      );
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    },
+  };
+  return task;
+}
+
+const task1 = newTask(
+  "Clean Cat Litter",
+  "Take all the 💩 out of the litter box"
+);
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+
